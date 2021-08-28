7f65ce900560a7a262a291d7b3e2f43e1e8e8b72
Ben Spotswood <spotswoodb@gmail.com>
2021-08-27 22:57:42 -0500

    commit tree 966d749b278242401ba1e4bda5a618f5acfe79f1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    coolDOMStuff();
+  });
+
+function coolDOMStuff(){
+    document.getElementById('text').innerHTML = 'This is really cool!'
+}
+
